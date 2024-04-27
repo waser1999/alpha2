7825d2f9aff58dfbfd4a7f7b0de16fa6937f8870
waser1999 <waser1999@outlook.com>
2024-04-27 21:39:06 +0800

    人物素材搭建

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D body;
+    
     void Start()
     {
-        Debug.Log("hello world");
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
