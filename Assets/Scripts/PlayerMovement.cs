5722cd7506d6c8e85365daef1a116b4848a52eee
waser1999 <waser1999@outlook.com>
2024-04-30 22:23:29 +0800

    跳跃基本逻辑

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
     public float walkSpeed;
-    
+    public float jumpForce;
+    public float jumpBuffer;
+
+    private float jumpBufferCounter;
+    private bool hasJumpBuffer = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,17 +28,52 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        Walk();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        bool jumpPressed = Input.GetAxisRaw("Jump") != 0;
+        bool onGround = true;
+
+        if(jumpPressed && !hasJumpBuffer && onGround)
         {
-            Walk(Input.GetAxis("Horizontal"));
+            hasJumpBuffer = true;
+            jumpBufferCounter = 0f;
+        }
+        
+        if(hasJumpBuffer)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpBufferCounter += Time.deltaTime;
+            
+            if(jumpBufferCounter > jumpBuffer)
+            {
+                hasJumpBuffer = false;
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+                rb.AddForce(Vector2.down * jumpForce * 2, ForceMode2D.Impulse);
+            }
         }
     }
 
-    private void Walk(float horizontal)
+    private void Walk()
     {
-        float xVelocity = horizontal * walkSpeed * Time.deltaTime;
-        rb.velocity = new Vector2 (xVelocity, rb.velocity.y);
+        float direction = Input.GetAxisRaw("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal");
 
-        transform.localScale = new Vector3(Math.Sign(xVelocity) * -1, transform.localScale.y, transform.localScale.z);
+        if (direction != 0)
+        {
+            float xVelocity = horizontal * walkSpeed * Time.deltaTime;
+            rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+            transform.localScale = new Vector3(direction * -1, transform.localScale.y, transform.localScale.z);
+
+            animator.SetBool("isWalk", true);
+        }
+        else if (rb.velocity.x == 0)
+        {
+            animator.SetBool("isWalk", false);
+        }
     }
 }
